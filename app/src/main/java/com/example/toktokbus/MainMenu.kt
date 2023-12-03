795f48b7792a2c430b84043e9aba6d523b0e5fb3
yerin03 <kyr2532@soongsil.ac.kr>
2023-12-04 00:38:43 +0900

    feat: 메인화면에서 버스지정 intent 구현

--- app/src/main/java/com/example/toktokbus/MainMenu.kt
+++ app/src/main/java/com/example/toktokbus/MainMenu.kt
@@ -1,4 +1,21 @@
 package com.example.toktokbus
 
-class MainMenu {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
+
+        val buttonBus = findViewById<LinearLayout>(R.id.btn_bus)
+        buttonBus.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
