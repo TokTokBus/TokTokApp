03dc0e01749002c7e92dd64f65409bb62388bd4c
yerin03 <kyr2532@soongsil.ac.kr>
2023-12-04 16:08:05 +0900

    feat: 카메라 기능 구현

--- app/src/main/java/com/example/toktokbus/RecBus.kt
+++ app/src/main/java/com/example/toktokbus/RecBus.kt
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.widget.LinearLayout
 
 class RecBus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.rec_bus)
+
+        val startCamButton: Button = findViewById(R.id.startcambtn)
+        startCamButton.setOnClickListener {
+            val intent = Intent(this, CameraTest::class.java)
+            startActivity(intent)
+        }
 
     }
 }
